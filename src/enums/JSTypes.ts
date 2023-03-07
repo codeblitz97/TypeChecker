4cd1b22dcf9c3d91a2464752707c764663f42588
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-03-07 14:49:01 +0600

    Create JsTypes enum

---
+++ src/enums/JSTypes.ts
@@ -0,0 +1,12 @@
+enum JsTypes {
+  String = "string",
+  Number = "number",
+  Boolean = "boolean",
+  Undefined = "undefined",
+  Null = "null",
+  Symbol = "symbol",
+  Object = "object",
+  Function = "function",
+}
+
+export { JsTypes };
---
+++ src/enums/index.ts
@@ -0,0 +1,1 @@
+export { JsTypes } from "./JSTypes";
